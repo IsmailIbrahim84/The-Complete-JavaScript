51783810cab29660941de4160c9f0b5edd99c584
IsmailIbrahim84 <beisso75@hotmail.com>
2020-02-04 10:35:53 +0200

    Array Coding Challenge 3

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-var 
+var bills = [124,48,168];
+var tips = [];
+var paidAmount = [];
+
+function tipCalculator (bill)
+{
+    var percentage;
+    if (bill <50)
+    {
+        percentage = 0.20;
+    }else if (bill >= 50 && bill <= 200) {
+        percentage = 0.15;
+    }
+    else
+    {
+        percentage=0.10;
+    }
+    return percentage;
+}
+
+bills.forEach(element => {
+paidAmount.push(element * tipCalculator(element));
+tips.push(tipCalculator(element));
+});
+
+console.log(paidAmount);
+console.log(tips);
